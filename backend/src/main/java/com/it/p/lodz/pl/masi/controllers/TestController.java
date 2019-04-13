f3c3458a3dcc859e47b170e75ed50e457aa8e299
Michal Malec <228021@edu.p.lodz.pl>
2019-04-13 12:49:37 +0200

    MAS19VI-42 implementacja PU T.14 #time 30m

--- backend/src/main/java/com/it/p/lodz/pl/masi/controllers/TestController.java
+++ backend/src/main/java/com/it/p/lodz/pl/masi/controllers/TestController.java
@@ -1,11 +1,14 @@
 package com.it.p.lodz.pl.masi.controllers;
 
 
+import com.it.p.lodz.pl.masi.dtos.TestVersionDto;
 import com.it.p.lodz.pl.masi.model.Test;
 import com.it.p.lodz.pl.masi.services.TestService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class TestController {
@@ -20,5 +23,10 @@ public class TestController {
     @GetMapping("/test")
     public Test getTestByTestVersionId(@RequestParam(value = "id") long id) {
         return testService.getTestByTestVersionId(id);
+    }
+
+    @GetMapping("/candidate/list/test")
+    public List<TestVersionDto> getTestListForCandidate(@RequestParam(value = "positionId") long positionId, @RequestParam(value = "languageId") long languageId) {
+        return testService.getTestListForCandidate(positionId, languageId);
     }
 }
--- backend/src/main/java/com/it/p/lodz/pl/masi/services/TestService.java
+++ backend/src/main/java/com/it/p/lodz/pl/masi/services/TestService.java
@@ -1,25 +1,46 @@
 package com.it.p.lodz.pl.masi.services;
 
+import com.it.p.lodz.pl.masi.dtos.TestVersionDto;
+import com.it.p.lodz.pl.masi.entities.TestEntity;
+import com.it.p.lodz.pl.masi.entities.TestVersionEntity;
 import com.it.p.lodz.pl.masi.model.Test;
 import com.it.p.lodz.pl.masi.repositories.LanguageRepository;
+import com.it.p.lodz.pl.masi.repositories.PositionRepository;
 import com.it.p.lodz.pl.masi.repositories.TestRepository;
 import com.it.p.lodz.pl.masi.repositories.TestVersionRepository;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
 import org.springframework.stereotype.Component;
+
+import java.lang.reflect.Type;
+import java.util.List;
 
 @Component
 public class TestService {
     private TestVersionRepository testVersionRepository;
     private TestRepository testRepository;
     private LanguageRepository languageRepository;
+    private PositionRepository positionRepository;
+    private ModelMapper modelMapper;
 
-    public TestService(TestVersionRepository testVersionRepository, TestRepository testRepository, LanguageRepository languageRepository) {
+    public TestService(TestVersionRepository testVersionRepository, TestRepository testRepository, LanguageRepository languageRepository, PositionRepository positionRepository, ModelMapper modelMapper) {
         this.testVersionRepository = testVersionRepository;
         this.testRepository = testRepository;
         this.languageRepository = languageRepository;
+        this.positionRepository = positionRepository;
+        this.modelMapper = modelMapper;
     }
 
     //TODO: Review or delete
     public Test getTestByTestVersionId(Long id) {
         return testVersionRepository.getOne(id).getTest();
+    }
+
+    public List<TestVersionDto> getTestListForCandidate(Long positionId, Long languageId) {
+        List<TestEntity> tests = testRepository.getAllByPositionByPositionIdAndActiveTrueAndDeletedFalse(positionRepository.getOne(positionId));
+        List<TestVersionEntity> testVersions = testVersionRepository.
+                getAllByLanguageByLanguageIdAndTestByTestIdAndActiveTrueAndDeletedFalse(languageRepository.getOne(languageId), tests);
+        Type listType = new TypeToken<List<TestVersionDto>>() {}.getType();
+        return modelMapper.map(testVersions, listType);
     }
 }
