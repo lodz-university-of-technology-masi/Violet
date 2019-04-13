a3aa9aac2a1fe8d730711343a95e0fc607aa2d9f
Michal Malec <228021@edu.p.lodz.pl>
2019-04-13 17:04:12 +0200

    MAS19VI-90 Swagger API documentation.

--- backend/src/main/java/com/it/p/lodz/pl/masi/configuration/SpringConfiguration.java
+++ backend/src/main/java/com/it/p/lodz/pl/masi/configuration/SpringConfiguration.java
@@ -1,4 +1,4 @@
-package com.it.p.lodz.pl.masi.confiiguration;
+package com.it.p.lodz.pl.masi.configuration;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
