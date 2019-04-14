28e644d5818221d448de719f9dd55d84befde3d1
228038 <48532600+228038@users.noreply.github.com>
2019-04-14 16:08:17 +0200

    Get active positions (#4)
    * Get active positions
    
    * Update PU13 URI

---
+++ backend/src/main/java/com/it/p/lodz/pl/masi/controllers/PositionController.java
@@ -0,0 +1,22 @@
+package com.it.p.lodz.pl.masi.controllers;
+
+import com.it.p.lodz.pl.masi.dtos.PositionDto;
+import com.it.p.lodz.pl.masi.services.PositionService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class PositionController {
+    private PositionService positionService;
+
+    public PositionController(PositionService positionService) {
+        this.positionService = positionService;
+    }
+
+    @GetMapping("/candidate/positions")
+    public List<PositionDto> getActivePositions() {
+        return positionService.getAllActivePositions();
+    }
+}
