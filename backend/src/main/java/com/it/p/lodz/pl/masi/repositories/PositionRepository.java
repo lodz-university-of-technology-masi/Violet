9a2b0fd79ab7f3ee520a925b505f5198034e7555
Pawel Purgat <purgat.pawel@gmail.com>
2019-04-14 15:30:40 +0200

    Get active positions

--- backend/src/main/java/com/it/p/lodz/pl/masi/repositories/PositionRepository.java
+++ backend/src/main/java/com/it/p/lodz/pl/masi/repositories/PositionRepository.java
@@ -2,6 +2,11 @@ package com.it.p.lodz.pl.masi.repositories;
 
 import com.it.p.lodz.pl.masi.entities.PositionEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface PositionRepository extends JpaRepository<PositionEntity, Long> {
+    @Query(value = "SELECT * FROM position WHERE is_active", nativeQuery = true)
+    List<PositionEntity> findAllActive();
 }
---
+++ backend/src/main/java/com/it/p/lodz/pl/masi/services/PositionService.java
@@ -0,0 +1,26 @@
+package com.it.p.lodz.pl.masi.services;
+
+import com.it.p.lodz.pl.masi.dtos.PositionDto;
+import com.it.p.lodz.pl.masi.repositories.PositionRepository;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class PositionService {
+    private PositionRepository positionRepository;
+    private ModelMapper mapper;
+
+    public PositionService(PositionRepository positionRepository, ModelMapper mapper) {
+        this.positionRepository = positionRepository;
+        this.mapper = mapper;
+    }
+
+    public List<PositionDto> getAllActivePositions() {
+        var positions = positionRepository.findAllActive();
+        var listType = new TypeToken<List<PositionDto>>(){}.getType();
+        return mapper.map(positions, listType);
+    }
+}
