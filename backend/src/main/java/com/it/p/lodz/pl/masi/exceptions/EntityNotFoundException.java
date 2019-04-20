53e1d3d35ec5e03043e52fc96f66730dcc795566
228038 <48532600+228038@users.noreply.github.com>
2019-04-20 21:15:40 +0200

    Exception change. (#13)

--- backend/src/main/java/com/it/p/lodz/pl/masi/exceptions/EntityNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.it.p.lodz.pl.masi.exceptions;
-
-import org.springframework.http.HttpStatus;
-
-public class EntityNotFoundException extends RuntimeException   {
-    public EntityNotFoundException(String message) {
-        super(message);
-    }
-}
