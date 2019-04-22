c14d1af1f8fd6f10c32b789bd9ae1bf4786fbf60
Michal Malec <228021@edu.p.lodz.pl>
2019-04-22 14:19:18 +0200

    MAS19VI-40 Rejestracja kandydata przed przejściem testu.

---
+++ backend/src/main/java/com/it/p/lodz/pl/masi/dtos/RegisterCandidateDto.java
@@ -0,0 +1,49 @@
+package com.it.p.lodz.pl.masi.dtos;
+
+public class RegisterCandidateDto {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String languageId;
+    private String positionId;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getLanguageId() {
+        return languageId;
+    }
+
+    public void setLanguageId(String languageId) {
+        this.languageId = languageId;
+    }
+
+    public String getPositionId() {
+        return positionId;
+    }
+
+    public void setPositionId(String positionId) {
+        this.positionId = positionId;
+    }
+}
---
+++ backend/src/main/java/com/it/p/lodz/pl/masi/dtos/RegisterCandidateResponseDto.java
@@ -0,0 +1,13 @@
+package com.it.p.lodz.pl.masi.dtos;
+
+public class RegisterCandidateResponseDto {
+    private String token;
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+}
