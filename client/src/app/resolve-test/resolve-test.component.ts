b14c69a350672298f43fa0802f144f6bc54a9feb
228038 <purgat.pawel@gmail.com>
2019-04-26 17:37:15 +0200

    Some CSS stuff

--- client/src/app/resolve-test/resolve-test.component.ts
+++ client/src/app/resolve-test/resolve-test.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { TestService } from '../shared/services/test.service';
 import { TestVersionContentModel, QuestionModel } from '../shared/model/test-model';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { FormGroup } from '@angular/forms';
 import { ResolveTestModel } from '../shared/model/resolve-test-model';
 
@@ -17,7 +17,7 @@ export class ResolveTestComponent implements OnInit {
   questions: QuestionModel[] = [];
   answers: string[];
 
-  constructor(private testService: TestService, private route: ActivatedRoute) { }
+  constructor(private testService: TestService, private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit() {
     this.route.queryParamMap.subscribe(p =>
@@ -45,7 +45,6 @@ export class ResolveTestComponent implements OnInit {
   }
 
   public resolveTest() {
-    console.log("resolved i guess");
     var test: ResolveTestModel = new ResolveTestModel();
     test.answers = this.answers;
     test.candidateToken = this.token;
@@ -53,8 +52,15 @@ export class ResolveTestComponent implements OnInit {
 
     this.testService.resolveTest(test)
     .subscribe(
-      t => console.log("yea, success"),
-      e => console.error(e.message));
+      t => {
+        console.log("yea, success");   
+        alert("OK"); //TODO: prompt success
+        this.router.navigate(['/home']);
+       }, 
+      e => {
+        console.error(e.message);
+        alert("Error");
+      });   //TODO: prompt error
   }
 
 }
