39418f9268f42e8165f196922b110e3be886113a
Michal Malec <228021@edu.p.lodz.pl>
2019-04-28 10:53:32 +0200

    MASI19VI-102 implementacja ngx-translate.

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,26 +1,28 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { PositionsService } from './shared/services/positions.service';
-import { HttpClientModule } from '@angular/common/http';
-import { AppComponent } from './app.component';
-import { PositionsListComponent } from './positions-list/positions-list.component';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {PositionsService} from './shared/services/positions.service';
+import {HttpClient, HttpClientModule} from '@angular/common/http';
+import {AppComponent} from './app.component';
+import {PositionsListComponent} from './positions-list/positions-list.component';
+import {TranslateHttpLoader} from '@ngx-translate/http-loader';
 import {
   MatButtonModule, MatCardModule, MatInputModule, MatListModule, MatGridListModule, MatRadioModule,
   MatToolbarModule, MatMenuModule, MatTableModule, MatPaginatorModule, MatSortModule, MatSelectModule
 } from '@angular/material';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { RouterModule, Routes } from '@angular/router';
-import { PositionAddComponent } from './position-add/position-add.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {RouterModule, Routes} from '@angular/router';
+import {PositionAddComponent} from './position-add/position-add.component';
 import {HomeComponent} from './home/home.component';
 import {Configuration} from './configuration';
-import { RegisterCandidateComponent } from './register-candidate/register-candidate.component';
+import {RegisterCandidateComponent} from './register-candidate/register-candidate.component';
 import {CandidateService} from './shared/services/candidate.service';
-import { ResolveTestComponent } from './resolve-test/resolve-test.component';
-import { TestService } from './shared/services/test.service';
+import {ResolveTestComponent} from './resolve-test/resolve-test.component';
+import {TestService} from './shared/services/test.service';
+import {TranslateLoader, TranslateModule} from '@ngx-translate/core';
 
 const appRoutes: Routes = [
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
   {
     path: 'positions-list',
     component: PositionsListComponent
@@ -38,51 +40,63 @@ const appRoutes: Routes = [
     component: RegisterCandidateComponent
   },
   {
-    path: "resolve-test",
+    path: 'resolve-test',
     component: ResolveTestComponent
   }
 ];
 
+export function createTranslateLoader(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
+
 @NgModule({
-   declarations: [
-      AppComponent,
-      PositionsListComponent,
-      PositionAddComponent,
-      HomeComponent,
-      PositionAddComponent,
-      RegisterCandidateComponent,
-      ResolveTestComponent
-   ],
-   imports: [
-      BrowserModule,
-      HttpClientModule,
-      BrowserAnimationsModule,
-      MatButtonModule,
-      MatCardModule,
-      MatInputModule,
-      MatListModule,
-      MatTableModule,
-      MatPaginatorModule,
-      MatMenuModule,
-      MatToolbarModule,
-      MatSelectModule,
-      MatGridListModule,
-      FormsModule,
-      ReactiveFormsModule,
-      MatSortModule,
-      MatRadioModule,
-      RouterModule.forRoot(appRoutes)
-   ],
-   providers: [
-      PositionsService,
-      Configuration,
-      CandidateService,
-      TestService
-   ],
-   bootstrap: [
-      AppComponent
-   ]
+  declarations: [
+    AppComponent,
+    PositionsListComponent,
+    PositionAddComponent,
+    HomeComponent,
+    PositionAddComponent,
+    RegisterCandidateComponent,
+    ResolveTestComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCardModule,
+    MatInputModule,
+    MatListModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatMenuModule,
+    MatToolbarModule,
+    MatSelectModule,
+    MatGridListModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatSortModule,
+    MatRadioModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: createTranslateLoader,
+        deps: [HttpClient]
+      }
+    }),
+    RouterModule.forRoot(appRoutes)
+  ],
+  providers: [
+    PositionsService,
+    Configuration,
+    CandidateService,
+    TestService
+  ],
+  bootstrap: [
+    AppComponent
+  ]
 })
-export class AppModule { }
+export class AppModule {
+}
 
 
--- client/src/app/shared/services/positions.service.ts
+++ client/src/app/shared/services/positions.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs/Observable';
+import { Observable } from 'rxjs';
 import {NewTestPosition, TestPosition} from '../model/position-model';
 import {Configuration} from '../../configuration';
 
