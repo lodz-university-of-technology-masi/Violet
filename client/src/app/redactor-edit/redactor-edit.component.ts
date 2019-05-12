69965b274c2e69b16eac13d0c060668f863fc643
228038 <purgat.pawel@gmail.com>
2019-05-12 12:40:49 +0200

    Return to redactor list after editing a redactor

--- client/src/app/redactor-edit/redactor-edit.component.ts
+++ client/src/app/redactor-edit/redactor-edit.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { RedactorModel } from '../shared/model/redactor-model';
 import { RedactorService } from '../shared/services/redactor.service';
 import { RedactorListComponent } from '../redactor-list/redactor-list.component';
 import { MessageService } from '../shared/services/message.service';
+import { routerNgProbeToken } from '@angular/router/src/router_module';
 
 @Component({
   selector: 'app-redactor-edit',
@@ -14,7 +15,7 @@ export class RedactorEditComponent implements OnInit {
   public redactorId: number;
   public redactor: RedactorModel;
 
-  constructor(private route: ActivatedRoute, private redactorService: RedactorService, private messageService: MessageService) { }
+  constructor(private route: ActivatedRoute, private redactorService: RedactorService, private messageService: MessageService, private router: Router) { }
 
   ngOnInit() {
     this.route.queryParamMap.subscribe(p => {
@@ -27,7 +28,10 @@ export class RedactorEditComponent implements OnInit {
 
   submitForm() {
     this.redactorService.editRedactor(this.redactorId, this.redactor)
-    .subscribe(s => this.messageService.success("redactor_saved"),
+    .subscribe(s => {
+        this.messageService.success("redactor_saved");
+        this.router.navigate(['/redactor-list']);
+      },
     e => this.messageService.error("redactor_edit_error"));
   }
 }
