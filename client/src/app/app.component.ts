d6d55e1a2ad93dfb7b124fb8e12edcadfc71ca34
204028 Wiktorek Paweł <204028@edu.p.lodz.pl>
2019-05-18 18:21:41 +0200

    pomiar dystansu myszy

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -6,6 +6,7 @@ import {AuthService} from './shared/services/auth.service';
 import {MessageService} from './shared/services/message.service';
 import {TestListWithVersions} from './shared/model/test-model';
 import {DeviceDetectorModule, DeviceDetectorService} from 'ngx-device-detector';
+import {isNotNullOrUndefined} from 'codelyzer/util/isNotNullOrUndefined';
 
 const languages = ['pl', 'en'];
 
@@ -22,11 +23,14 @@ export class AppComponent {
   };
   isLogged = false;
   UserRole = UserRole;
-  timeLeft: number = 0;
+  time = 0;
   interval;
   startTimerValue = true;
   deviceInfo = '';
   clicksCounter = 0;
+  distance = 0;
+  currentX;
+  currentY;
 
   constructor(private router: Router, private translateService: TranslateService, private authService: AuthService,
               private messageService: MessageService, private deviceService: DeviceDetectorService) {
@@ -143,20 +147,22 @@ export class AppComponent {
 
   startTimer() {
     this.interval = setInterval(() => {
-      this.timeLeft++;
+      this.time++;
     }, 100);
   }
 
   pauseTimer() {
     clearInterval(this.interval);
-    this.timeLeft = this.timeLeft / 10;
-    this.saveDataInStorage(this.timeLeft, this.clicksCounter, this.deviceInfo);
+    this.time = this.time / 10;
+    let integerDistance = Math.trunc(this.distance);
+    this.saveDataInStorage(this.time, this.clicksCounter, integerDistance, this.deviceInfo);
   }
 
-  saveDataInStorage(time: number, clicks: number, browserName: String) {
+  saveDataInStorage(time: number, clicks: number, distance: number, browserName: String) {
     const jsonData = {
       'time': time,
       'clicks': clicks,
+      'distance': distance,
       'browser': browserName
     };
     if (typeof (Storage) !== 'undefined') {
@@ -174,6 +180,7 @@ export class AppComponent {
     if (event.key === 'D'.valueOf() && event.shiftKey && this.startTimerValue === true) {
       this.startTimerValue = false;
       this.clicksCounter = 0;
+      this.distance = 0;
       this.startTimer();
       this.messageService.info('measurement_started');
     } else if (event.key === 'D'.valueOf() && event.shiftKey && this.startTimerValue === false) {
@@ -186,9 +193,19 @@ export class AppComponent {
   @HostListener('click', ['$event'])
   onMouseEnter(event: any) {
     this.clicksCounter++;
+    let x = event.clientX;     // Get the horizontal coordinate
+    let y = event.clientY;     // Get the vertical coordinate
+    if(isNotNullOrUndefined(this.currentY) && isNotNullOrUndefined(this.currentX)){
+      this.distance += this.countDistance(this.currentX,this.currentY,x,y);
+    }
+    this.currentX = x;
+    this.currentY = y;
   }
 
   getBrowser() {
     this.deviceInfo = this.deviceService.browser.charAt(0);
+  }
+  countDistance(x1: number, y1: number, x2: number, y2: number): number{
+    return  Math.sqrt( Math.pow((x1-x2), 2) + Math.pow((y1-y2), 2) );
   }
 }
